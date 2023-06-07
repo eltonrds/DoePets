b36e598579571b550966a6daa4475218a69bedaa
Elton Rodrigues de Souza <120459876+eltonrds@users.noreply.github.com>
2023-06-07 20:33:23 -0300

    altercação2

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let menu = document.getElementById("links-navegacao");
+let menu_barras = document.getElementById("menu_barras");
+
+function mostrarMenu (){
+    if (window.getComputedStyle(menu).display == "none") {
+        menu.style.display = "flex"
+        menu_barras.setAttribute("aria-label", "fechar menu")
+        menu_barras.setAttribute("aria-expanded", "true")
+    }else{
+        menu.style.display = "none"
+        menu_barras.setAttribute("aria-label", "abrir menu")
+        menu_barras.setAttribute("aria-expanded", "false")
+    }
+}
